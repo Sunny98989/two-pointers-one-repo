e20f09d5e48ebd7b42738feaa052c3e51aa04845
Sunny Prajapati <111737881+Sunny98989@users.noreply.github.com>
2024-11-27 03:28:52 +0000

    [LeetCode Sync] Runtime - 30 ms (99.57%), Memory - 17.3 MB (100.00%)

--- leetcode/1894-merge-strings-alternately/solution.py
+++ leetcode/1894-merge-strings-alternately/solution.py
@@ -1,19 +1,13 @@
-class Solution(object):
-    def mergeAlternately(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: str
-        """
-        len1 = len(word1)
-        len2 = len(word2)
-        maxlen = max(len1,len2)
-        merged = []
-        i = 0
-        for i in range(maxlen):
-            if i < len1:
-                merged.append(word1[i])
-            if i < len2:
-                merged.append(word2[i])
-
-        return "".join(merged)
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        output = []
+        l = min(len(word1),len(word2))
+        for i in range(l):
+            output.append(word1[i])  
+            output.append(word2[i])
+        if len(word1)>l:
+            output.append(word1[l:])
+        if len(word2)>l:
+            output.append(word2[l:])
+            
+        return "".join(output)
