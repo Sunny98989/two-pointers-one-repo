6fa7de4d2f8771fb4fb781f574ddd0e463fb1af7
Sunny Prajapati <111737881+Sunny98989@users.noreply.github.com>
2024-12-26 04:50:27 +0000

    [LeetCode Sync] Runtime - 1 ms (45.67%), Memory - 17.8 MB (100.00%)

--- leetcode/0020-valid-parentheses/solution.py
+++ leetcode/0020-valid-parentheses/solution.py
@@ -1,23 +1,23 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = list()
+        stack = []
         for char in s:
             if not stack:
-                if char in (')', '}', ']'):
+                if char in (")","]","}"):
                     return False
                 else:
                     stack.append(char)
             else:
-                if char in (')', '}', ']'):
-                    if stack[-1] == '(' and char == ')' or stack[-1] == '{' and char == '}' or stack[-1] == '[' and char == ']' :
+                if char in (")","}","]"):
+                    if stack[-1]=='(' and char == ')' or stack[-1]=='[' and char == ']' or stack[-1]=='{' and char == '}':
                         stack.pop()
                     else:
                         return False
                 else:
                     stack.append(char)
-                    
         if not stack:
             return True
         else:
             return False
-        
+                
+            
