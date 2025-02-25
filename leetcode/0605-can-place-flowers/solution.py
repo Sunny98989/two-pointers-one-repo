6a44485d05a7e526a37027e5d4756e671392a7e2
Sunny Prajapati <111737881+Sunny98989@users.noreply.github.com>
2025-02-25 03:34:26 +0000

    [LeetCode Sync] Runtime - 7 ms (79.36%), Memory - 18.1 MB (100.00%)

--- leetcode/0605-can-place-flowers/solution.py
+++ leetcode/0605-can-place-flowers/solution.py
@@ -1,15 +1,9 @@
-class Solution(object):
-    def canPlaceFlowers(self, flowerbed, n):
-        """
-        :type flowerbed: List[int]
-        :type n: int
-        :rtype: bool
-        """
-        extended_flowerbed = [0]+flowerbed+[0]
-        for i in range(1,len(flowerbed) + 1):
-            if extended_flowerbed[i-1:i+2] == [0,0,0]:
-                extended_flowerbed[i] = 1
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        f = [0]+ flowerbed + [0]
+        for i in range(1,len(flowerbed)+1):
+            if f[i] == 0 and f[i+1] == 0 and f[i-1] == 0:
+                f[i] = 1
                 n-=1
-        return n<=0
         
-
+        return n <= 0
