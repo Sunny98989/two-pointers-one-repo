cb7c4881e9a45de3ec5d9d35736675563d4063f6
Sunny Prajapati <111737881+Sunny98989@users.noreply.github.com>
2025-03-17 15:57:18 +0000

    [LeetCode Sync] Runtime - 1 ms (17.94%), Memory - 17.6 MB (100.00%)

--- leetcode/1528-kids-with-the-greatest-number-of-candies/solution.py
+++ leetcode/1528-kids-with-the-greatest-number-of-candies/solution.py
@@ -1,9 +1,3 @@
-class Solution(object):
-    def kidsWithCandies(self, candies, extraCandies):
-        """
-        :type candies: List[int]
-        :type extraCandies: int
-        :rtype: List[bool]
-        """
-        greatest = max(candies)
-        return [True if i + extraCandies >= greatest else False for i in candies]
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        return [(candy+extraCandies)>=max(candies) for candy in candies]
