ec935e78f50d515cafbfb4e0eb5ee6b5a1be8856
Sunny Prajapati <111737881+Sunny98989@users.noreply.github.com>
2025-03-17 15:55:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (100.00%)

--- leetcode/1146-greatest-common-divisor-of-strings/solution.py
+++ leetcode/1146-greatest-common-divisor-of-strings/solution.py
@@ -1,16 +1,11 @@
-class Solution(object):
-    def gcdOfStrings(self, str1, str2):
-        """
-        :type str1: str
-        :type str2: str
-        :rtype: str
-        """
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1+str2 != str2+str1:
+            return ""
         len1 = len(str1)
         len2 = len(str2)
-        if str1 + str2 != str2 + str1:
-            return ""
         if len1 == len2:
             return str1
-        if len1 > len2:
+        if len1>len2:
             return self.gcdOfStrings(str1[len2:], str2)
-        return self.gcdOfStrings(str1, str2[len1:])
+        return self.gcdOfStrings(str1,str2[len1:])
