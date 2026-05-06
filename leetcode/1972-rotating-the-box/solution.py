2835f5f06c50c1b0bd6e41b915af039afc363027
Sunny Prajapati <111737881+Sunny98989@users.noreply.github.com>
2026-05-06 16:46:34 +0000

    [LeetCode Sync] Runtime - 123 ms (58.67%), Memory - 57 MB (17.66%)

--- leetcode/1972-rotating-the-box/solution.py
+++ leetcode/1972-rotating-the-box/solution.py
@@ -1,15 +1,15 @@
 class Solution:
     def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
-        rows = len(box)
-        cols = len(box[0])
+        rows = len(box) #1
+        cols = len(box[0])#3
         
-        for r in range(rows):
-            empty_pos = cols - 1
-            for c in range(cols - 1, -1, -1):
+        for r in range(rows): #0
+            empty_pos = cols - 1 #2 #1
+            for c in range(cols - 1, -1, -1): #2, -1
                 if box[r][c] == '#': 
                     box[r][c], box[r][empty_pos] = box[r][empty_pos], box[r][c]
-                    empty_pos -= 1
-                elif box[r][c] == '*': 
+                    empty_pos -= 1  
+                elif box[r][c] == '*':
                     empty_pos = c - 1
         
         rotated_box = []
